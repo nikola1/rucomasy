19efe098124a983387693fa1a8545a0ccb0f338b
Nikola Taushanov <nktaushanov@gmail.com>
2013-01-31 01:40:17 +0200

    Fix Task's constructor.

--- core/tasks/task.rb
+++ core/tasks/task.rb
@@ -2,11 +2,12 @@ class Task
   attr_reader :statement, :type, :test_cases, :checker, :runner
 
   def initialize(description = {})
-    @statement  = description.fetch(:statement, "")
-    @type       = description.fetch(:type, :standart)
-    @test_cases = description.fetch(:test_cases, [])
-    @checker    = description.fetch(:checker, Checker)
-    @runner     = description.fetch(:runner, Runner)
+    @statement  = description.fetch :statement,  ""
+    @type       = description.fetch :type,       Types::STANDART
+    @test_cases = description.fetch :test_cases, []
+    @checker    = description.fetch :checker,    Checker
+    @runner     = description.fetch :runner,     Runner
+    @rule       = description.fetch :rule,       Rules::DEFAULT
+    @limits     = description.fetch :limits,     Limits::DEFAULT
   end
 end
-
