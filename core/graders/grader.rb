40c5519cb27c5d5b7156ed3f0a7c6bafc9a8a53e
Nikola Taushanov <nktaushanov@gmail.com>
2013-02-02 01:55:01 +0200

    Fix issue with a wrong file location from the last commit.

---
+++ core/graders/grader.rb
@@ -0,0 +1,26 @@
+require 'immutable_struct'
+
+module Grader
+  # Result = ImmutableStruct.new(:status, :message, :runtime, :exitcode, :memory, :points)
+  class Result
+    attr_reader :status, :message, :runtime, :exitcode, :memory, :points
+
+    def initialize(status, message = "", runtime = -1, exitcode = 0, memory = -1, points = 0)
+      @status   = status
+      @message  = message
+      @runtime  = runtime
+      @exitcode = exitcode
+      @memory   = memory
+      @points   = points
+    end
+
+    module Status
+      CE = :ce
+      RE = :re
+      OK = :ok
+      TL = :tl
+      ML = :ml
+      UE = :ue
+    end
+  end
+end
