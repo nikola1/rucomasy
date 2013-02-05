cef1d21ced298587e924574b7f951d9273d9042f
Nikola Taushanov <nktaushanov@gmail.com>
2013-02-05 19:24:33 +0200

    Extends the interface of the Compiler class.

--- core/compilers/compiler.rb
+++ core/compilers/compiler.rb
@@ -1,49 +1,18 @@
 require 'open3'
-require_relative '../executors/runnable.rb'
 
 class Compiler
-  class << self
-    def compile(source, executable, additional_files = [])
-      destination = File.join(create_directory, executable)
+  def compile(source, executable, additional_files = [])
+    destination = File.join(create_directory, executable)
 
-      before_compilation source, destination, additional_files
+    before_compilation(source, destination, additional_files)
 
-      compile_command = prepare_compile_command source, destination, additional_files
+    compile_command = prepare_compile_command(source, destination, additional_files)
+    status = Status.new *Open3.capture3(*compile_command)
 
-      status = Status.new *Open3.capture3(*compile_command)
+    after_compilation(source, destination, additional_files)
 
-      after_compilation source, destination, additional_files
-
-      command = create_command executable, additional_files
-      return status, Runnable.new(command, File.dirname(destination))
-    end
-
-    private
-
-    def before_compilation(source, destination, additional_files)
-    end
-
-    def after_compilation(source, destination, additional_files)
-    end
-
-    def create_command(executable, additional_files)
-      "./#{executable}"
-    end
-
-    def prepare_compile_command(source, destination, additional_files)
-      COMMAND_OPTIONS.map do |option|
-        option.gsub /%SOURCE%|%DESTINATION%/,
-          '%SOURCE%' => source, '%DESTINATION%' => destination
-      end
-    end
-
-    protected
-
-    def create_directory
-      directory_name = "../tmp/#{Time.now.to_i}_#{Process.pid}"
-      FileUtils.mkdir_p directory_name if not File.exists? directory_name
-      directory_name
-    end
+    command = create_command(executable, additional_files)
+    return status, create_runnable(command, destination, additional_files)
   end
 
   class Status
@@ -53,5 +22,34 @@ class Compiler
       @output, @error = output, error
       @success = status.success?
     end
+  end
+
+  private
+
+  def before_compilation(source, destination, additional_files)
+  end
+
+  def prepare_compile_command(source, destination, additional_files)
+    COMMAND_OPTIONS.map do |option|
+      option.gsub /%SOURCE%|%DESTINATION%/,
+        '%SOURCE%' => source, '%DESTINATION%' => destination
+    end
+  end
+
+  def after_compilation(source, destination, additional_files)
+  end
+
+  def create_command(executable, additional_files)
+    "./#{executable}"
+  end
+
+  def create_runnable(command, destination, additional_files)
+    Runnable.new(command, File.dirname(destination))
+  end
+
+  def create_directory
+    directory_name = "../tmp/#{Time.now.to_i}_#{Process.pid}"
+    FileUtils.mkdir_p directory_name if not File.exists? directory_name
+    directory_name
   end
 end
