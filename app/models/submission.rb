190ad29825f3eca9baf7e7019f1fea8b34cd8fe5
Nikola Taushanov <nktaushanov@gmail.com>
2013-02-21 23:00:58 +0200

    Remove 'tested' property in submission's model. There are three different states (status) for each submission - 'not tested'->'testing'->'tested'.

--- app/models/submission.rb
+++ app/models/submission.rb
@@ -1,12 +1,11 @@
 class Submission
   include DataMapper::Resource
 
-  property :id,         Serial,  serial: true
-  property :tested,     Boolean, required: true, default: false
-  property :status,     String
+  property :id,         Serial, serial: true
+  property :status,     String, required: true, default: 'not tested'
   property :message,    String
   property :log,        Text
-  property :result,     Float,   default: 0.0
+  property :result,     Float,  default: 0.0
   property :created_at, DateTime
 
   belongs_to :task
