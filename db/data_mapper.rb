a4e9e1a76a9045e987679893177dbbeee6f985ac
Nikola Taushanov <nktaushanov@gmail.com>
2013-02-23 02:36:39 +0200

    Fix the datamapper loading.

--- db/data_mapper.rb
+++ db/data_mapper.rb
@@ -1,9 +1,9 @@
 require 'data_mapper'
 
 # DataMapper::Logger.new(STDOUT, :debug)
-DataMapper.setup(:default, "sqlite://#{File.join File.dirname(__FILE__), 'development.db'}")
+DataMapper.setup(:default, "sqlite://#{Rucomasy::FileHelper::DB}/development.db}")
 
-RucomasyBase.require_files_from RucomasyBase::MODELS
+Rucomasy::FileHelper.require_files_from Rucomasy::FileHelper::MODELS
 
 DataMapper.auto_upgrade!
 DataMapper.finalize
