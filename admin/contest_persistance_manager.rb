e3b353a417677de24bfe12b25ca35ff287ba21cd
Nikola Taushanov <nktaushanov@gmail.com>
2013-02-23 04:04:12 +0200

    Put rat's classes in Rucomasy::Rat module.

--- admin/contest_persistance_manager.rb
+++ admin/contest_persistance_manager.rb
@@ -1,22 +1,26 @@
-class ContestPersistanceManager < PersistanceManager
-  def initialize
-    @object_type = Contest
+module Rucomasy
+  module Rat
+    class ContestPersistanceManager < PersistanceManager
+      def initialize
+        @object_type = Contest
 
-    @deleted     = "Contest was successfully deleted!"
-    @not_deleted = "The contest was not deleted."
-    @saved       = "Contest saved successfuly."
-    @not_saved   = "Contest not saved."
-    @not_exists  = "Such a contest doesn't exist."
-  end
+        @deleted     = "Contest was successfully deleted!"
+        @not_deleted = "The contest was not deleted."
+        @saved       = "Contest saved successfuly."
+        @not_saved   = "Contest not saved."
+        @not_exists  = "Such a contest doesn't exist."
+      end
 
-  def print(contest)
-    puts "Name: #{contest.name}".ljust(40) + "Start date: #{contest.start_date}"
-    puts "Contest id: #{contest.id}".ljust(40) + "End date:   #{contest.end_date}"
-    unless contest.description.nil?
-      puts "-" * CHARS_PER_LINE
-      puts "Description:"
-      puts contest.description.chars.each_slice(CHARS_PER_LINE).map { |x| x.join.lstrip }.join "\n"
+      def print(contest)
+        puts "Name: #{contest.name}".ljust(40) + "Start date: #{contest.start_date}"
+        puts "Contest id: #{contest.id}".ljust(40) + "End date:   #{contest.end_date}"
+        unless contest.description.nil?
+          puts "-" * CHARS_PER_LINE
+          puts "Description:"
+          puts contest.description.chars.each_slice(CHARS_PER_LINE).map { |x| x.join.lstrip }.join "\n"
+        end
+        puts "=" * CHARS_PER_LINE
+      end
     end
-    puts "=" * CHARS_PER_LINE
   end
 end
--- admin/task_persistance_manager.rb
+++ admin/task_persistance_manager.rb
@@ -1,22 +1,26 @@
-class TaskPersistanceManager < PersistanceManager
-  def initialize
-    @object_type = Task
+module Rucomasy
+  module Rat
+    class TaskPersistanceManager < PersistanceManager
+      def initialize
+        @object_type = Task
 
-    @deleted     = "Task was successfully deleted!"
-    @not_deleted = "The task was not deleted."
-    @saved       = "Task saved successfuly."
-    @not_saved   = "Task not saved."
-    @not_exists  = "Such a task doesn't exist."
-  end
+        @deleted     = "Task was successfully deleted!"
+        @not_deleted = "The task was not deleted."
+        @saved       = "Task saved successfuly."
+        @not_saved   = "Task not saved."
+        @not_exists  = "Such a task doesn't exist."
+      end
 
-  def print(task)
-    puts "Name: #{task.name}".ljust(40) + "Runner:  #{task.runner}"
-    puts "Task id: #{task.id}#{', Task: ' + task.contest.name if task.contest}".ljust(40) + "Checker: #{task.checker}"
-    unless task.statement.nil?
-      puts "-" * CHARS_PER_LINE
-      puts "Statement:"
-      puts task.statement.chars.each_slice(CHARS_PER_LINE).map { |x| x.join.lstrip }.join "\n"
+      def print(task)
+        puts "Name: #{task.name}".ljust(40) + "Runner:  #{task.runner}"
+        puts "Task id: #{task.id}#{', Task: ' + task.contest.name if task.contest}".ljust(40) + "Checker: #{task.checker}"
+        unless task.statement.nil?
+          puts "-" * CHARS_PER_LINE
+          puts "Statement:"
+          puts task.statement.chars.each_slice(CHARS_PER_LINE).map { |x| x.join.lstrip }.join "\n"
+        end
+        puts "=" * CHARS_PER_LINE
+      end
     end
-    puts "=" * CHARS_PER_LINE
   end
 end
